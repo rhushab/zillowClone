51d462b4566cefeb7af99c4441b28481c20b607f
rhushab <rhushabhbontapallee@googlemail.com>
2023-12-22 13:11:46 -0400

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
+import mongoose from 'mongoose';
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('CONNECTED!');
+  })
+  .catch(() => {
+    console.log('ERROR!');
+  });
 const app = express();
 app.listen(3000, () => {
   console.log('running on 3000');
